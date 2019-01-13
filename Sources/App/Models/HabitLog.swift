e0e49f8235121df06f6dcae0284219354c55a5ff
Luciano Sclovsky <luciano.sclovsky@softdesign-rs.com.br>
2019-01-13 17:28:37 -0200

    Added HabitLog post route

---
+++ Sources/App/Models/HabitLog.swift
@@ -0,0 +1,20 @@
+import Vapor
+import FluentSQLite
+
+final class HabitLog: Codable {
+    var id: Int?
+    var date: String
+    var done: Bool
+    
+    init(date: String, done: Bool) {
+        self.date = date
+        self.done = done
+    }
+}
+
+extension HabitLog: SQLiteModel {}
+
+extension HabitLog: Migration {}
+
+extension HabitLog: Content {}
+
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,6 +2,7 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
+    
     // Basic "It works" example
     router.get { req in
         return "It works!"
@@ -11,10 +12,13 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.post("api", "habitlogs") { req -> Future<HabitLog> in
+    return try req.content.decode(HabitLog.self)
+        .flatMap(to: HabitLog.self) { habitLog in
+            return habitLog.save(on: req)
+        }
+    }
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+
 }
